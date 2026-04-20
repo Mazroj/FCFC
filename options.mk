e1df55184938b55620bbb0b6a379c209ac800d1b
mazroj <jordan.zambrano@ug.uchile.cl>
2026-04-20 12:22:58 -0400

    init_commit

--- options.mk
+++ options.mk
@@ -10,7 +10,7 @@ CFLAGS = -O3 -march=native
 CFLAGS += -flto=auto
 
 # Set `WITH_MPI := T` to enable MPI.
-WITH_MPI := 
+WITH_MPI :=T
 
 # Set `WITH_OMP := T` to enable OpenMP.
 # The compiler flag for OpenMP can be set via `OMP_FLAG`.
@@ -22,22 +22,22 @@ OMP_FLAG := -fopenmp
 WITH_SIMD := T
 
 # Set `SINGLE_PREC := T` to enable single-precision.
-SINGLE_PREC := 
+SINGLE_PREC := T
 
 # Set `WITH_MU_ONE := T` to include (s,mu) pairs with mu = 1.
-WITH_MU_ONE := 
+WITH_MU_ONE := T
 
 # Set `WITH_CFITSIO := T` to enable FITS file format.
 # The paths for cfitsio header (fitsio.h) and library (libcfitsio.{a,so,dylib})
 # files can be set via `CFITSIO_INC_DIR` and `CFITSIO_LIB_DIR`, respectively.
-WITH_CFITSIO := 
+WITH_CFITSIO := T
 CFITSIO_INC_DIR := 
 CFITSIO_LIB_DIR := 
 
 # Set `WITH_HDF5 := T` to enable HDF5 file format.
 # The paths for libhdf5 header (hdf5.h) and library (libhdf5.{a,so,dylib})
 # files can be set via `HDF5_INC_DIR` and `HDF5_LIB_DIR`, respectively.
-WITH_HDF5 := 
+WITH_HDF5 := T
 HDF5_INC_DIR := 
 HDF5_LIB_DIR := 
 
